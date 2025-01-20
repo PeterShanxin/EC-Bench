ab5a356104b4c6f45f63d0b190f10393786aa132
davoudisaeedeh <saeedeh.davoudi@ucdenver.edu>
2025-01-19 19:14:05 -0700

    add ECPred

---
+++ ECPred/run_model.sh
@@ -0,0 +1,2 @@
+java -jar ECPred.jar weighted ../data/price-149.fasta ../ECPred/ temp/ results_price.tsv
+java -jar ECPred.jar weighted ../data/test_task3.fasta ../ECPred/ temp/ results_test_task3.tsv
