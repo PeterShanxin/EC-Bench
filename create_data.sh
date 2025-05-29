f66b8c0e4236ae69f5d7dd8425e88129fba47550
saeedehdavoudi <d.saeedeh74@gmail.com>
2025-05-29 01:29:42 -0600

    modified:   create_data.sh

--- create_data.sh
+++ create_data.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 code/create_data.py --ensemble_file_name ensemble.csv &> create_data.log
+python3 code/create_data.py --ensemble_file_name ensemble.csv --threshold 30 &> create_data_30.log
+python3 code/create_data.py --ensemble_file_name ensemble.csv --threshold 100 &> create_data_100.log
