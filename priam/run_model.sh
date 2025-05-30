b30c0cf516f6f3d504df56fe4a161295706a5a83
saeedehdavoudi <d.saeedeh74@gmail.com>
2025-05-30 09:08:57 -0600

    new file:   CatFam/run_model.sh deleted:    priam/paj_ens-100_params.ppf deleted:    priam/paj_ens-30_params.ppf deleted:    priam/paj_price-149_params.ppf deleted:    priam/paj_price_params.ppf deleted:    priam/paj_test_params.ppf deleted:    priam/paj_test_task3_params.ppf modified:   priam/run_model.sh

--- priam/run_model.sh
+++ priam/run_model.sh
@@ -8,7 +8,6 @@ mkdir -p $BLAST_DIR
 if [ ! -f "$BLAST_DIR/blastall" ]; then
     echo "Installing BLAST 2.2.26..."
 
-    cd /tmp
     wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/release/2.2.26/ncbi-blast-2.2.26-x64-linux.tar.gz
     tar -xzf ncbi-blast-2.2.26-x64-linux.tar.gz
     mv ncbi-blast-2.2.26/* $BLAST_DIR
@@ -21,6 +20,22 @@ fi
 
 # Add to PATH (optional, if not already)
 export PATH=$BLAST_DIR:$PATH
+
+# Set up PRIAM directory
+PRIAM_DIR=priam
+mkdir -p $PRIAM_DIR
+
+# Download and unzip PRIAM if not already present
+if [ ! -f "$PRIAM_DIR/README.txt" ]; then
+    echo "Downloading PRIAM..."
+    wget -O PRIAM_Distribution.zip "https://web.archive.org/web/20160806022844/http://priam.prabi.fr/REL_MAR13/Distribution.zip"
+    unzip PRIAM_Distribution.zip -d $PRIAM_DIR
+    rm PRIAM_Distribution.zip
+    echo "PRIAM extracted to $PRIAM_DIR"
+else
+    echo "PRIAM already exists in $PRIAM_DIR"
+fi
+
 
 cd priam
 java -jar PRIAM_search.jar -n "price-149" -i ../data/price-149.fasta -p PRIAM_MAR13 -o "../results/priam" --pt 0 --mp 60 --cc T --bd ../blast-2.2.26/bin --np 112
